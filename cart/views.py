91d3c3605c1e7a256d560eb6a7aa3ae1278e5a22
George <glychock@gmail.com>
2022-02-01 12:14:12 -0500

    app: Created add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
-    """ View to return index.html """
+    """ View contents of the user's cart """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+    """ from Code Institute, Django Module https://codeinstitute.net/global/ """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
