bf36f314cbb16e6fbfd4321bd05af1b67c2db61d
George <glychock@gmail.com>
2022-02-11 12:03:47 -0500

    feature: Customized allauth templates and updated view for profile

--- spilt_wine/settings.py
+++ spilt_wine/settings.py
@@ -84,8 +84,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'spilt_wine/templates'),
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            # os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'spilt_wine/templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
