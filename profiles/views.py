c7c2ded0f4d077d08dc66f7e22b20272e35e338f
George <glychock@gmail.com>
2022-02-13 20:24:34 -0500

    fix: Updated all image url mu with condition to check for an image file present

--- profiles/views.py
+++ profiles/views.py
@@ -16,24 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-    
-    # Attempt to prefill the form with any info the user maintains in their profile
-    if request.user.is_authenticated:
-        try:
-            profile = UserProfile.objects.get(user=request.user)
-            form = UserProfileForm(initial={
-                'phone_number': profile.default_phone_number,
-                'country': profile.default_country,
-                'postcode': profile.default_postcode,
-                'town_or_city': profile.default_town_or_city,
-                'street_address1': profile.default_street_address1,
-                'street_address2': profile.default_street_address2,
-                'county': profile.default_county,
-            })
-        except UserProfile.DoesNotExist:
-            form = UserProfileForm()
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
     orders = profile.orders.all()
 
